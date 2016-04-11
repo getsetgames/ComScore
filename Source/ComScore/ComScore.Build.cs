30c1120ac3d742d94d7b5161409160b26205da6c
Robert Segal <rob@getsetgames.com>
2016-04-11 12:01:59 -0400

    ComScore build script

---
+++ Source/ComScore/ComScore.Build.cs
@@ -0,0 +1,90 @@
+//
+//  Created by Robert Segal on 2016-04-07.
+//  Copyright (c) 2016 Get Set Games Inc. All rights reserved.
+//
+
+using System.IO;
+
+namespace UnrealBuildTool.Rules
+{
+	public class ComScore : ModuleRules
+	{
+		private string ModulePath
+		{
+			get { return Path.GetDirectoryName( RulesCompiler.GetModuleFilename( this.GetType().Name ) ); }
+		}
+
+		public ComScore(TargetInfo Target)
+		{
+			PublicIncludePaths.AddRange(
+				new string[] {
+					// ... add public include paths required here ...
+				}
+				);
+
+			PrivateIncludePaths.AddRange(
+				new string[] {
+					"Developer/ComScore/Private",
+					// ... add other private include paths required here ...
+				}
+				);
+
+			PublicDependencyModuleNames.AddRange(
+				new string[]
+				{
+					"Core",
+					"CoreUObject",
+					"Engine"
+					// ... add other public dependencies that you statically link with here ...
+				}
+				);
+
+			PrivateDependencyModuleNames.AddRange(
+				new string[]
+				{
+					// ... add private dependencies that you statically link with here ...
+				}
+				);
+
+			DynamicallyLoadedModuleNames.AddRange(
+				new string[]
+				{
+					// ... add any modules that your module loads dynamically here ...
+				}
+				);
+				
+			PrivateIncludePathModuleNames.AddRange(
+			new string[] {
+				"Settings"
+			}
+			);
+
+
+			if (Target.Platform == UnrealTargetPlatform.IOS) {
+
+				var LibDir = Path.Combine(ModulePath,"..","..","lib","iOS");
+				PrivateIncludePaths.Add(LibDir);
+
+				PublicAdditionalLibraries.Add(Path.Combine(LibDir,"libcomScore.a"));
+
+				PublicFrameworks.AddRange( 
+					new string[] 
+					{ 
+						"AdSupport",
+						"StoreKit",
+						"CFNetwork",
+						"SystemConfiguration",
+						"UIKit",
+						"Foundation",
+						"CoreGraphics"
+					}
+				);
+			}
+			else if(Target.Platform == UnrealTargetPlatform.Android)
+			{
+				string PluginPath = Utils.MakePathRelativeTo(ModuleDirectory, BuildConfiguration.RelativeEnginePath);
+				AdditionalPropertiesForReceipt.Add(new ReceiptProperty("AndroidPlugin", Path.Combine(PluginPath, "ComScore_APL.xml")));
+			}
+		}
+	}
+}
