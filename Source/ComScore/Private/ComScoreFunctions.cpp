57d6cd916570eccd0a2abfb136acde5c86f0fa07
Robert Segal <rob@getsetgames.com>
2016-04-12 16:48:29 -0400

    Startup/initialization required with a bit of logging and error checking for iOS

--- Source/ComScore/Private/ComScoreFunctions.cpp
+++ Source/ComScore/Private/ComScoreFunctions.cpp
@@ -5,13 +5,46 @@
 
 #include "ComScorePrivatePCH.h"
 
-#if PLATFORM_ANDROID
+#if PLATFORM_IOS
+#include "CSComScore.h"
+#elif PLATFORM_ANDROID
+#include "Android/AndroidJNI.h"
+#include "AndroidApplication.h"
+#endif
 
-// #include "Android/AndroidJNI.h"
-// #include "AndroidApplication.h"
+#if PLATFORM_IOS
+
+@interface ComScoreFunctionsDelegate : NSObject
+@end
 
 
-#elif PLATFORM_IOS
+@implementation ComScoreFunctionsDelegate
 
+-(void)applicationDidFinishLaunching:(NSNotification *)Notification
+{
+    const UComScoreSettings* DefaultSettings = GetDefault<UComScoreSettings>();
+    
+    NSString* c2id       = DefaultSettings->C2ID.GetNSString();
+    NSString* secretCode = DefaultSettings->SecretCode.GetNSString();
+    NSString* appName    = DefaultSettings->AppName.GetNSString();
+    
+    if (c2id && secretCode && appName)
+    {
+        [CSComScore setAppContext];
+        
+        [CSComScore setCustomerC2:c2id];
+        [CSComScore setPublisherSecret:secretCode];
+        [CSComScore setAppName:appName];
+    }
+    else
+    {
+        UE_LOG(LogComScore, Log, TEXT("Error with c2id, secret code or app name"));
+    }
+}
+
+@end
+
+#elif PLATFORM_ANDROID
 
 #endif
+
