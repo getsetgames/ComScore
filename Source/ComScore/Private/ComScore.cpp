21ba47ea5ea4600387781fd436795d4daee40cfc
Robert Segal <rob@getsetgames.com>
2016-06-07 15:11:10 -0400

    Formatting cleanup

--- Source/ComScore/Private/ComScore.cpp
+++ Source/ComScore/Private/ComScore.cpp
@@ -140,7 +140,6 @@ void FComScore::StartupModule()
 	}
 }
 
-
 void FComScore::ShutdownModule()
 {
 	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
